d39d759b8c2984109a8e12c6e243836667dd57da
Ahmad-Abulaban <eng.ahmad090@gmail.com>
2021-09-25 23:20:28 +0300

    All Done

--- Module/Movie.js
+++ Module/Movie.js
@@ -1,32 +1,37 @@
 const axios = require("axios");
 let cacheMemory = {};
 function handleMovie(request, response) {
-    let { searchQuery} = request.query;
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
-    if (cacheMemory[searchQuery] !== undefined) {
-        console.log('the cashe contain data ')
-        console.log(cacheMemory);
-        response.send(cacheMemory[searchQuery]);
-    } else { console.log('cache memory is empty hit the api')
-    try{
-    axios.get(url).then(results => {
-        const moviesArray = results.data.results.map(movie => new Movie(movie));
+  let { searchQuery } = request.query;
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
+  if (cacheMemory[searchQuery] !== undefined) {
+    console.log("the cashe contain data ");
+    console.log(cacheMemory);
+    response.send(cacheMemory[searchQuery]);
+  } else {
+    console.log("cache memory is empty hit the api");
+    try {
+      axios.get(url).then((results) => {
+        const moviesArray = results.data.results.map(
+          (movie) => new Movie(movie)
+        );
         cacheMemory[searchQuery] = moviesArray;
         response.status(200).send(moviesArray);
-    })
-} catch(error){
-    console.log('somethink happend', error);
-}}}
-class Movie{
-    constructor(movie){
-        this.title = movie.title;
-        this.overview = movie.overview;
-        this.averageVotes = movie.vote_average;
-        this.totalVotes = movie.vote_count;
-        this.imageUrl = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
-        this.popularity = movie.popularity;
-        this.releasedOn = movie.release_date;
+      });
+    } catch (error) {
+      console.log("somethink happend", error);
     }
+  }
+}
+class Movie {
+  constructor(movie) {
+    this.title = movie.title;
+    this.overview = movie.overview;
+    this.averageVotes = movie.vote_average;
+    this.totalVotes = movie.vote_count;
+    this.imageUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+    this.popularity = movie.popularity;
+    this.releasedOn = movie.release_date;
+  }
 }
 
 module.exports = handleMovie;
--- Module/Weather.js
+++ Module/Weather.js
@@ -1,30 +1,33 @@
 const axios = require("axios");
 let cacheMemory = {};
 function handleWeather(request, response) {
-    let { city} = request.query;
-    const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city},NC&key=${process.env.WEATHER_API_KEY}`;
-    if (cacheMemory[city] !== undefined) {
-        
-        response.send(cacheMemory[city]);
-    } else { 
-        try{
-    axios.get(url).then(results => {
-        const WeatherArray = results.data.data.map(weather => new Weather(weather));
+  let { city } = request.query;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${process.env.WEATHER_API_KEY}`;
+  if (cacheMemory[city] !== undefined) {
+    console.log("the cashe contain data ");
+    console.log(cacheMemory);
+    response.send(cacheMemory[city]);
+  } else { 
+      console.log("cache memory is empty hit the api");
+    try {
+      axios.get(url).then((results) => {
+        const WeatherArray = results.data.data.map(
+          (weather) => new Weather(weather)
+        );
         cacheMemory[city] = WeatherArray;
         response.status(200).send(WeatherArray);
-    })
-    } catch(error){
-       console.log('something happend', error);
+      });
+    } catch (error) {
+      console.log("something happend", error);
+    }
   }
 }
-    }
 
-class Weather{
-    constructor(weather){
-        this.date = weather.datetime;
-        this.description = weather.weather.description;
-        
-    }
+class Weather {
+  constructor(weather) {
+    this.date = weather.datetime;
+    this.description = weather.weather.description;
+  }
 }
 
 module.exports = handleWeather;
